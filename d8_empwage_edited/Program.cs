28e8e9cf5350419112b2bf4212689b01e7c4fd64
D-Meshach <meshachdaniel456@gmail.com>
2023-02-27 03:39:57 +0530

    Employee Attendance added

---
+++ d8_empwage_edited/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d8_empwage_edited
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Wage Problem");
+            empwage_process.empAttendance();
+
+        }
+    }
+}
