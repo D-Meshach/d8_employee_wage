c99247327736afaffc5b98821c7a7d2c901e4516
D-Meshach <meshachdaniel456@gmail.com>
2023-02-27 05:31:56 +0530

    conditions added

--- d8_empwage_edited/emp_process.cs
+++ d8_empwage_edited/emp_process.cs
@@ -8,19 +8,16 @@ namespace d8_empwage_edited
 {
     internal class empwage_process
     {
-
-
-
-
-
-
         public static void empAttendance(Random random)
         {
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
             int workHr = 0;
             int totalEmpWage = 0;
-            for (int i = 0; i < 20; i++) {
+            int totalWorkHr = 0;
+            int totalDays = 0;
+            while (totalWorkHr<100 && totalDays<20) {
+                totalDays++;
                 int empCheck = random.Next(0,3);
                 switch (empCheck)
                 {
@@ -30,14 +27,18 @@ namespace d8_empwage_edited
                     default: workHr = 0; break;
 
                 }
+                
+                totalWorkHr += workHr;
                 int current_wage = empDailyWage(workHr);
                 totalEmpWage += current_wage;
                 Console.WriteLine("Emp Wage: " + current_wage);
                 
             }
             Console.WriteLine("The Total Employee Working Wage :" + totalEmpWage);
-            
-            
+            Console.WriteLine("The Total Employee Working Days :" + (totalDays));
+            Console.WriteLine("The Total Employee Working Hour :" + (totalWorkHr));
+
+
 
         }
         public static int empDailyWage(int empWorkHr)
